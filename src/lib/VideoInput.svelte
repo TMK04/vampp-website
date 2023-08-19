59c2d7b3155d90b95af1a187520100319a9a923a
TMK04 <floer.learner.01@gmail.com>
2023-08-19 10:43:45 +0800

    add video preview

--- src/lib/VideoInput.svelte
+++ src/lib/VideoInput.svelte
@@ -3,6 +3,7 @@
 	import VideoIcon from "$lib/VideoIcon.svelte";
 
 	let file: File;
+	$: src = file ? URL.createObjectURL(file) : "";
 
 	function handleFileChange(event: Event) {
 		const target = event.target as HTMLInputElement;
@@ -27,8 +28,12 @@
 	class="border-border relative flex aspect-video cursor-pointer items-center justify-center rounded-sm border border-dashed bg-background p-10 text-center text-xl leading-tight text-secondary"
 	for="input-video"
 >
+	{#if file}
+		<!-- svelte-ignore a11y-media-has-caption -->
+		<video class="absolute left-0 top-0 h-full w-full rounded-sm object-cover" controls {src} />
+	{/if}
 	<header
-		class="border-border absolute left-0 top-0 flex items-center gap-1.5 rounded-br-sm border-b border-r border-opacity-50 py-0.5 pl-1 pr-1.5 text-xs shadow-sm"
+		class="border-border absolute left-0 top-0 flex items-center gap-1.5 rounded-br-sm border-b border-r border-opacity-50 bg-background bg-opacity-95 py-0.5 pl-1 pr-1.5 text-xs shadow-sm"
 	>
 		<VideoIcon class="inline" size={12} />
 		<span>Input Video</span>
