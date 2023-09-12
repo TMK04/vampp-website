4976bdde547ab4e7eb60121ba419eb70a87a5346
TMK04 <floer.learner.01@gmail.com>
2023-09-13 01:45:52 +0800

    refactor(+page.server): set timeout to 5 minutes

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -5,9 +5,13 @@ export const actions: Actions = {
 	receive_video: async ({ request }) => {
 		const formData = await request.formData();
 		console.log(formData);
+		const controller = new AbortController();
+		// 5 minutes timeout
+		setTimeout(() => controller.abort(), 5 * 60 * 1000);
 		const response = await fetch(HOST, {
 			method: "POST",
-			body: formData
+			body: formData,
+			signal: controller.signal
 		});
 
 		if (!response.ok) {
