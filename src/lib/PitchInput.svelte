ac7a867b8678a47e15f0c082a1361cab210c544e
TMK04 <floer.learner.01@gmail.com>
2023-09-13 15:11:46 +0800

    change PitchInput to textarea

--- src/lib/PitchInput.svelte
+++ src/lib/PitchInput.svelte
@@ -5,11 +5,10 @@
 </script>
 
 <Fieldset id="input-pitch" label="Pitch">
-	<input
+	<textarea
 		class="block w-full rounded-sm border border-border px-1 py-1.5"
 		id="input-pitch"
 		name="pitch"
-		type="text"
 		required
 		bind:value={pitch}
 	/>
