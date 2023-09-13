09b73e105d9d022bd755bb794524af9e804bc0cd
TMK04 <floer.learner.01@gmail.com>
2023-09-13 15:06:18 +0800

    denest body & add props individually

--- src/lib/ConversationTabPicker/index.svelte
+++ src/lib/ConversationTabPicker/index.svelte
@@ -30,8 +30,22 @@
 		body = JSON.parse(body);
 		const obj_id_conversation: ObjIdConversation = {};
 		for (const { id, ...conversation } of body) {
-			console.log(id);
-			obj_id_conversation[id] = conversation;
+			obj_id_conversation[id.S] = {
+				topic: conversation.topic.S,
+				file: conversation.file.S,
+				pitch: conversation.pitch.S,
+				pe: conversation.pe.N,
+				clarity: conversation.clarity.N,
+				bv: conversation.bv.N,
+				beholder_creativity: conversation.beholder_creativity.N,
+				beholder_creativity_justification: conversation.beholder_creativity_justification.S,
+				beholder_impact: conversation.beholder_impact.N,
+				beholder_impact_justification: conversation.beholder_impact_justification.S,
+				beholder_feasibility: conversation.beholder_feasibility.N,
+				beholder_feasibility_justification: conversation.beholder_feasibility_justification.S,
+				beholder_clarity: conversation.beholder_clarity.N,
+				beholder_clarity_justification: conversation.beholder_clarity_justification.S
+			};
 		}
 
 		obj_id_conversation_store.set(obj_id_conversation);
