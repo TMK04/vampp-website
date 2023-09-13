9cb4e455c3d9107095ce3892975b500037407d92
TMK04 <floer.learner.01@gmail.com>
2023-09-13 13:56:41 +0800

    feat(+page.server): add getHistories action

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,25 @@
 import { HOST } from "$env/static/private";
 import { error, type Actions } from "@sveltejs/kit";
 
+async function handleFastApiError(response: Response) {
+	console.error(response);
+	const body = await response
+		.json()
+		.then((data) => data["detail"])
+		.catch(() => response.statusText);
+	throw error(response.status, body);
+}
+
 export const actions: Actions = {
+	getHistories: async () => {
+		const response = await fetch(`${HOST}/histories`);
+		if (!response.ok) {
+			await handleFastApiError(response);
+		}
+		const body = await response.json();
+		console.log(body);
+		return JSON.stringify(body);
+	},
 	receive_video: async ({ request }) => {
 		const formData = await request.formData();
 		console.log(formData);
@@ -16,12 +34,7 @@ export const actions: Actions = {
 		clearTimeout(timeout);
 
 		if (!response.ok) {
-			console.error(response);
-			const body = await response
-				.json()
-				.then((data) => data["detail"])
-				.catch(() => response.statusText);
-			throw error(response.status, body);
+			await handleFastApiError(response);
 		}
 		const body = await response.json();
 		console.log(body);
