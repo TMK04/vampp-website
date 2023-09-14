7272c9fb12cf4bc273fa4d1993de4088c0888787
TMK04 <floer.learner.01@gmail.com>
2023-09-14 18:43:18 +0800

    refactor(Container): round more

--- src/lib/Container.svelte
+++ src/lib/Container.svelte
@@ -4,4 +4,4 @@
 	export { className as class };
 </script>
 
-<section class="rounded-sm bg-light p-2 {className}"><slot /></section>
+<section class="rounded bg-light p-2 {className}"><slot /></section>
