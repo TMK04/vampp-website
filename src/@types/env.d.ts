0946e51d5e35028cdd036a309d6566bee7a83901
TMK04 <floer.learner.01@gmail.com>
2023-09-18 01:55:50 +0800

    save file then send basename & random

--- src/@types/env.d.ts
+++ src/@types/env.d.ts
@@ -1,3 +1,5 @@
 declare module "$env/static/private" {
 	export const HOST: string;
+	export const TMP_DIR: string;
+	export const TMP_FILENAME: string;
 }
--- src/routes/+server.ts
+++ src/routes/+server.ts
@@ -1,5 +1,9 @@
-import { HOST } from "$env/static/private";
+import { HOST, TMP_DIR, TMP_FILENAME } from "$env/static/private";
 import { json } from "@sveltejs/kit";
+import { spawn } from "child_process";
+import { createWriteStream, existsSync, mkdirSync } from "fs";
+import { nanoid } from "nanoid";
+import { WritableStream } from "stream/web";
 import { handleFastApiError } from "../server-helpers";
 
 export async function GET() {
@@ -13,7 +17,45 @@ export async function GET() {
 
 export async function POST({ request }) {
 	const formData = await request.formData();
-	console.log(formData);
+	const file = formData.get("file") as File | string;
+	formData.delete("file");
+	const file_is_ytid = typeof file === "string";
+
+	const basename = file_is_ytid ? file : file.name;
+	let random: string;
+	let basename_random: string;
+	do {
+		random = nanoid(7);
+		basename_random = `${TMP_DIR}/${basename}-${random}`;
+	} while (existsSync(basename_random));
+
+	mkdirSync(basename_random);
+	basename_random = `${basename_random}/${TMP_FILENAME}.mp4`;
+	if (file_is_ytid) {
+		spawn("yt-dlp", [
+			"-f",
+			"bv[height<=1080][fps<=60]+ba",
+			"--merge-output-format",
+			"mp4",
+			"-o",
+			basename_random,
+			"--",
+			file
+		]);
+	} else {
+		const read_stream = file.stream();
+		const write_stream = createWriteStream(basename_random);
+		const writable_stream = new WritableStream({
+			write(chunk) {
+				write_stream.write(chunk);
+			}
+		});
+		await read_stream.pipeTo(writable_stream);
+		write_stream.end();
+	}
+
+	formData.append("basename", basename);
+	formData.append("random", random);
 	const controller = new AbortController();
 	// 5 minutes timeout
 	const timeout = setTimeout(() => controller.abort(), 5 * 60 * 1000);
