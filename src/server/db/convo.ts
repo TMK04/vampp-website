6ea80b9ce79083cd3e3aef0378cc485fa192c6f2
TMK04 <floer.learner.01@gmail.com>
2023-12-31 14:19:08 +0800

    refactor(db/convo): stricten schema types

--- src/server/db/convo.ts
+++ src/server/db/convo.ts
@@ -27,28 +27,28 @@ async function main() {
 	const convo_exists = await db.schema.hasTable("convo");
 	if (!convo_exists) {
 		await db.schema.createTable("convo", (table) => {
-			table.string("id").primary().notNullable();
-			table.integer("ts").notNullable();
-			table.string("topic").notNullable();
-			table.string("pitch").notNullable();
-			table.string("summary").notNullable();
-			table.float("pe");
-			table.float("clarity");
-			table.float("bv");
-			table.float("moving");
-			table.float("smiling");
-			table.float("upright");
-			table.float("ec");
-			table.float("speech_clarity");
-			table.float("speech_enthusiasm");
-			table.float("beholder_creativity");
-			table.string("beholder_creativity_justification");
-			table.float("beholder_feasibility");
-			table.string("beholder_feasibility_justification");
-			table.float("beholder_impact");
-			table.string("beholder_impact_justification");
-			table.float("beholder_clarity");
-			table.string("beholder_clarity_justification");
+			table.specificType("id", "CHAR(21)").primary().notNullable(); // https://www.npmjs.com/package/nanoid#:~:text=from%C2%A036%C2%A0to-,21%C2%A0symbols,-.
+			table.integer("ts").unsigned().notNullable();
+			table.string("topic", 100).notNullable(); // https://boldcontentvideo.com/2018/01/24/optimise-youtube-title-description-tags/#:~:text=YouTube%20Video%20Title,for%20your%20title
+			table.text("pitch").notNullable();
+			table.text("summary").notNullable();
+			table.float("pe").unsigned();
+			table.float("clarity").unsigned();
+			table.float("bv").unsigned();
+			table.float("moving").unsigned();
+			table.float("smiling").unsigned();
+			table.float("upright").unsigned();
+			table.float("ec").unsigned();
+			table.float("speech_clarity").unsigned();
+			table.float("speech_enthusiasm").unsigned();
+			table.float("beholder_creativity").unsigned();
+			table.text("beholder_creativity_justification");
+			table.float("beholder_feasibility").unsigned();
+			table.text("beholder_feasibility_justification");
+			table.float("beholder_impact").unsigned();
+			table.text("beholder_impact_justification");
+			table.float("beholder_clarity").unsigned();
+			table.text("beholder_clarity_justification");
 			table.boolean("pa");
 		});
 	}
