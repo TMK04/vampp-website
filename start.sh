0e5e6b6bb61431f502f2d05b008193cc152ab428
TMK04 <floer.learner.01@gmail.com>
2024-01-06 02:22:46 +0800

    remove proxy

--- start.sh
+++
@@ -1,3 +0,0 @@
-. .env
-
-HOST=$VITE_HOST PORT=$VITE_PORT node -r dotenv/config build/
--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,6 @@ import { defineConfig, loadEnv, type ConfigEnv } from "vite";
 export default function ({ mode }: ConfigEnv) {
 	process.env = Object.assign(process.env, loadEnv(mode, process.cwd()), "");
 	return defineConfig({
-		plugins: [sveltekit()],
-		server: {
-			host: process.env.VITE_HOST,
-			port: +(process.env.VITE_PORT ?? 8001),
-			strictPort: true,
-			cors: {
-				origin: process.env.VITE_PROXY_ENDPOINT,
-				credentials: true,
-				methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
-			}
-		}
+		plugins: [sveltekit()]
 	});
 }
