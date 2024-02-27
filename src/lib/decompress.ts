7a9fccac03b0a5bb33c08f37b15a94372e5746e7
TMK04 <floer.learner.01@gmail.com>
2024-02-27 14:01:45 +0800

    refactor(decompress): replace xz-decompress w/ lzma-web
    supports web workers, to not block UI thread

--- src/lib/decompress.ts
+++ src/lib/decompress.ts
@@ -1,35 +1,36 @@
 import { logError } from "$server/console";
-import xz_decompress from "xz-decompress";
-const { XzReadableStream } = xz_decompress;
+import LZMA from "lzma-web";
 
-async function decompress(uint8arr: Uint8Array): Promise<Uint8Array> {
-	const readable_stream = new Blob([uint8arr]).stream();
-	const xz_readable_stream = new XzReadableStream(readable_stream);
-	let decompressed_uint8arr = new Uint8Array();
-	const writable_stream = new WritableStream({
-		write(chunk) {
-			decompressed_uint8arr = new Uint8Array([...decompressed_uint8arr, ...chunk]);
-		}
-	});
-	await xz_readable_stream.pipeTo(writable_stream);
-	return decompressed_uint8arr;
+async function decompress(uint8arr: Uint8Array) {
+	const lzma = new LZMA();
+	const decompressed = await lzma.decompress(uint8arr);
+	return decompressed;
 }
 
 export async function decompressVideo(base64_str: string): Promise<string> {
 	try {
-		const binary_str = atob(base64_str);
-		const binary_l = binary_str.length;
-		const uint8arr = new Uint8Array(binary_l);
-		for (let i = 0; i < binary_l; i++) {
-			uint8arr[i] = binary_str.charCodeAt(i);
+		const uint8arr = base64ToUint8Arr(base64_str);
+
+		const decompressed = await decompress(uint8arr);
+		console.log("decompressed", decompressed);
+
+		if (decompressed instanceof Uint8Array) {
+			const decompressed_str = URL.createObjectURL(new Blob([decompressed]));
+			return decompressed_str;
 		}
-
-		const decompressed_uint8arr = await decompress(uint8arr);
-
-		const decompressed = URL.createObjectURL(new Blob([decompressed_uint8arr]));
 		return decompressed;
 	} catch (e) {
 		logError(e);
 		return "";
 	}
+}
+
+function base64ToUint8Arr(base64_str: string): Uint8Array {
+	const binary_str = atob(base64_str);
+	const binary_l = binary_str.length;
+	const uint8arr = new Uint8Array(binary_l);
+	for (let i = 0; i < binary_l; i++) {
+		uint8arr[i] = binary_str.charCodeAt(i);
+	}
+	return uint8arr;
 }
